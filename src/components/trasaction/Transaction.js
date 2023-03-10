081efbd4d9d8b84484fb81ffc435e4f37cbecb0a
Oksana-Hrebinnyk <110463912+Oksana-Hrebinnyk@users.noreply.github.com>
2023-03-10 19:34:48 +0200

    all componrnts

---
+++ src/components/trasaction/Transaction.js
@@ -0,0 +1,34 @@
+import PropTypes from 'prop-types';
+import css from './Transaction.module.css';
+
+export const Transaction = ({ items }) => (
+  <table className={css.transactionHistoryTable}>
+    <thead className={css.hederRow}>
+      <tr className={css.valuesRow}>
+        <th className={css.columnHeader}>Type</th>
+        <th className={css.columnHeader}>Amount</th>
+        <th className={css.columnHeader}>Currency</th>
+      </tr>
+    </thead>
+    <tbody>
+      {items.map(item => (
+        <tr key={item.id} className={css.valuesRow}>
+          <td className={css.transactionData}>{item.type}</td>
+          <td className={css.transactionData}>{item.amount}</td>
+          <td className={css.transactionData}>{item.currency}</td>
+        </tr>
+      ))}
+    </tbody>
+  </table>
+);
+
+Transaction.PropTypes = {
+  items: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string.isRequired,
+      type: PropTypes.string.isRequired,
+      amount: PropTypes.string.isRequired,
+      currency: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
