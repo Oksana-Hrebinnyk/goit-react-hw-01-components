0f7761be7439abcfadbb519bb6ef599b05359f5d
Oksana-Hrebinnyk <110463912+Oksana-Hrebinnyk@users.noreply.github.com>
2023-08-05 09:25:51 +0300

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,41 @@
+import { Profile } from './profile/Profile';
+import user from './profile/user.json';
+
+import { Statistics } from './statistics/Statistics';
+import data from './statistics/data.json';
+
+import { FriendList } from 'components/friend-list/FriendList';
+import friends from './friend-list/friends.json';
+
+import { Transaction } from './transaction/Transaction';
+import transactions from './transaction/transaction.json';
+
+
+
+
+export const App = () => {
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      <Statistics title="Upload stats" stats={data} />
+      <Statistics stats={data} />
+      <FriendList friends={friends} />
+      <Transaction items={transactions} />
+    </div>
+  );
+};
